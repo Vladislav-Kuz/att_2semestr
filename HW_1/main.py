38921fd848ab80b0c3129600a3cf86f089d69077
Vladislav-Kuz <136375272+Vladislav-Kuz@users.noreply.github.com>
2023-09-05 10:53:36 +0300

    hw

--- HW_1/main.py
+++ HW_1/main.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #1
 n = 123
 
@@ -51,4 +52,59 @@ n = 123
 # if var1 == var2:
 #     print('yes')
 # else:
+=======
+#1
+n = 123
+
+# m = str(n)
+# res = int(m[0]) + int(m[1]) + int(m[2])
+# print(res)
+
+# f1 = n//100
+# f2 = n%100//10
+# f3 = n%10
+# res = f1 + f2 + f3 
+# print(f"{res} -> {f1} {f2} {f3}")
+
+#2
+# n = 38
+# sum = p + k + s = x + 4x + x
+
+# if n%6==0:
+#     p = (n//6)
+#     s = (n//6)
+#     k = ((4*n)//6)
+#     print(f"{p} {k} {s}")
+# else: print("Это неправильные кораблики")
+
+#3
+# n = 323233
+# m = str(n)
+# if (int(m[0])+int(m[1])+int(m[2]))==(int(m[3])+int(m[4])+int(m[5])):
+#     print('yes')
+# else:
+#     print('no')
+
+#4
+# n = 2
+# m = 3
+# k = 2
+# if((k%n==0 or k%m==0) and k!=n*m):
+#     print('yes')
+# else:
+#     print('no')
+
+#3
+# n=567865
+# n1 = int(n)//1000
+# n2 = int(n)%1000
+# var1= n1//100 + (n1%100)//10 + n1%10
+# var2= n2//100 + (n2%100)//10 + n2%10
+
+# print(n1)
+# print(n2)
+# if var1 == var2:
+#     print('yes')
+# else:
+>>>>>>> c673da6 (hw)
 #     print('no')

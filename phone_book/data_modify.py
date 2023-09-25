fac03a97a451c55bb12b80b2600fe3468a1d2de0
Vladislav-Kuz <136375272+Vladislav-Kuz@users.noreply.github.com>
2023-09-26 01:19:45 +0300

    правки

--- phone_book/data_modify.py
+++ phone_book/data_modify.py
@@ -10,29 +10,40 @@ def modify_data():
             if searched in new_item:
                 doubled_data.append(new_item)  # список повторяющихся
                 data_to_preview.append(item)
-        if len(doubled_data) > 1:
+                flag =0
+            else:
+                flag = 1
+        if len(doubled_data) >= 1:
             for index in range(len(doubled_data)):
                 print(f"{index + 1} : {data_to_preview[index]}")
             print('Выберете контакт для изменения из списка: \n')
             index_variant = int(input(" ")) - 1
             to_modify = doubled_data.pop(index_variant)  # получили измененное поле (строку)
-        for item in data:
-            new_item = item.replace('\n', ' ').split()  # Это список из элементов контакта
-            if new_item != to_modify:
-                new_data.append(item)
-            else:
-                print('Выберете строку для изменения: \n'
-                      '1. Имя\n'
-                      '2. Фамилия\n'
-                      '3. Телефон\n'
-                      '4. Адрес')
-                index = int(input("Введите вариант: ")) - 1
-                to_modify = input("Введите новые данные: ").title()
-                for i in range(len(new_item)):
-                    if i == index:
-                        new_item[i] = to_modify
-                        new_data.append(new_item[0] + " " + new_item[1] + "\n" + new_item[2] + "\n" + new_item[3])
-                        print(f"Добавлены новые данные в контакт: \n{new_item[0]}  {new_item[1]}\n{new_item[2]}\n"
-                              f"{new_item[3]}")
-    with open("book.txt", 'w', encoding='utf8') as file:
-        file.write('\n\n'.join(new_data))
+            for item in data:
+                new_item = item.replace('\n', ' ').split()  # Это список из элементов контакта
+                # print(f"to mod {to_modify}")
+                # print(f"new item {new_item}")
+                if to_modify != new_item:
+                    new_data.append(item)
+                    # print(f"Добавляем {item}")
+                else:
+                    print('Выберете строку для изменения: \n'
+                            '1. Имя\n'
+                            '2. Фамилия\n'
+                            '3. Телефон\n'
+                            '4. Адрес')
+                    index = int(input("Введите вариант: ")) - 1
+                    to_modify = input("Введите новые данные: ").title()
+                    for i in range(len(new_item)):
+                        if i == index:
+                            new_item[i] = to_modify
+                            new_data.append(new_item[0] + " " + new_item[1] + "\n" + new_item[2] + "\n" + new_item[3])
+                            print(f"Добавлены новые данные в контакт: \n{new_item[0]}  {new_item[1]}\n{new_item[2]}\n"
+                                f"{new_item[3]}")
+    if flag == 0:
+        with open("book.txt", 'w', encoding='utf8') as file:
+            file.write('\n\n'.join(new_data))
+    else:
+        print("Такого контакта нет")
+
+
